5be0feb1c32391c513536f7f637f7c35a47e6c78
pbxnn <zxn920628@163.com>
2023-08-16 16:00:18 +0800

    init db

--- sql/c3h_module_data_map.sql
+++ sql/c3h_module_data_map.sql
@@ -1,2 +1,37 @@
+insert into `c3h_module_data_map` (`data_key`, `module_key`) values
+('R401S_MAPD_control_mode', 'R401S_switch_info'),
+('R401S_APC_main_switch', 'R401S_switch_info'),
+('R401S_APC_write_switch', 'R401S_switch_info'),
+('R401S_communication', 'R401S_switch_info'),
+('R401S_MAPD_control_mode', 'R401S_switch_info'),
 
+('R401S_cycle_materials_flow', 'R401S_operation_vars'),
+('R401S_hydrogen_ratio', 'R401S_operation_vars'),
+('R401S_ingress_temperature', 'R401S_operation_vars'),
+('R401S_hydrogen_flow', 'R401S_operation_vars'),
+('R401S_reactor_pressure', 'R401S_operation_vars'),
+('R401S_cycle_materials_flow_control_switch', 'R401S_operation_vars'),
+('R401S_hydrogen_ratio_control_switch', 'R401S_operation_vars'),
 
+('R401S_mixed_total_materials_flow', 'R401S_confounding_vars'),
+('R401S_fresh_materials_flow', 'R401S_confounding_vars'),
+('R401S_egress_temperature', 'R401S_confounding_vars'),
+
+('R401S_reactor_pressure_drop', 'R401S_status_vars'),
+('R401S_up_bed_temperature_AA', 'R401S_status_vars'),
+('R401S_up_bed_temperature_AB', 'R401S_status_vars'),
+('R401S_mid_bed_temperature_AA', 'R401S_status_vars'),
+('R401S_mid_bed_temperature_AB', 'R401S_status_vars'),
+('R401S_down_bed_temperature_AA', 'R401S_status_vars'),
+('R401S_down_bed_temperature_AB', 'R401S_status_vars'),
+
+('R401S_c3h6_selectivity', 'R401S_reactor_performance'),
+('R401S_MAPD_inversion_rate', 'R401S_reactor_performance'),
+('R401S_ingress_MAPD_analysis', 'R401S_reactor_performance'),
+('R401S_egress_MAPD_analysis', 'R401S_reactor_performance'),
+('R401S_ingress_c3h6_analysis', 'R401S_reactor_performance'),
+('R401S_egress_c3h6_analysis', 'R401S_reactor_performance'),
+('R401S_ingress_c3h8_analysis', 'R401S_reactor_performance'),
+('R401S_egress_c3h8_analysis', 'R401S_reactor_performance'),
+('R401S_ingress_c4', 'R401S_reactor_performance'),
+('R401S_egress_hydrogen', 'R401S_reactor_performance');
--- sql/c3h_modules.sql
+++ sql/c3h_modules.sql
@@ -1,10 +1,17 @@
 insert into `c3h_module_info` (`name`, `key`, `desc`) values
-('R401_APC_Controller','', 'R401 APC Controller'),
-('operation_vars', '','操作变量'),
-('confounding_vars', '','干扰变量'),
-('reactor_performance','', '反应器性能'),
-('s_bed_temperature', '','S床层温度');
-('开关信息', 'switch_info', '开关信息'),
-('被控变量', 'controlled_vars', '被控变量'),
-('控制变量', 'control_vars', '控制变量'),
-('催化剂性能', 'catalyst', '催化剂性能');
+-- ('R401_APC_Controller','', 'R401 APC Controller'),
+-- ('operation_vars', '','操作变量'),
+-- ('confounding_vars', '','干扰变量'),
+-- ('reactor_performance','', '反应器性能'),
+-- ('s_bed_temperature', '','S床层温度');
+-- ('开关信息', 'switch_info', '开关信息'),
+-- ('被控变量', 'controlled_vars', '被控变量'),
+-- ('控制变量', 'control_vars', '控制变量'),
+-- ('催化剂性能', 'catalyst', '催化剂性能');
+
+('开关信息', 'R401S_switch_info', '开关信息'),
+('操作变量', 'R401S_operation_vars','操作变量'),
+('干扰变量', 'R401S_confounding_vars','干扰变量'),
+('状态变量', 'R401S_status_vars','状态变量'),
+('反应器性能', 'R401S_reactor_performance', '反应器性能'),
+
